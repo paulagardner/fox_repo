caf2f4bb89e243d7a82543c1913d26ced52e1c88
Paula Gardner <paula.gardner@uea.ac.uk>
2024-10-16 09:51:06 +0100

    first commit for big batch alignment files. This took about 24 hours on the SLURM settings specified

---
+++ readgroups/readgroups_wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#path to read groups config file:
+TXTPATH="/gpfs/data/bergstrom/foxseq2024/read-group-config.txt"
+
+#Load bwa and samtools:
+module load bwa
+module load samtools
+
+#Loop over files, submit separate run-bwa.sh job for each file - here using the "head" command to get only the first line, for testing purposes:
+mkdir -p slurm-out
+
+#removing |head -1 | pipe from after sed command, as I want to run these all!
+sed 1d "$TXTPATH" | while read file ID SM LB PL; do \
+sbatch -J batch_align --time=3-0 --mem=10G --cpus-per-task=9 --partition=compute-64-512 -o slurm-out/out_bwa.$ID.o -e slurm-out/out_bwa.$ID.e \
+--wrap="./run-bwa.sh /gpfs/data/bergstrom/foxseq2024/$file $ID $SM $LB $PL"; \
+done
+#each bwa command needs 10GB, but the runtime will depend on the size of the input file. Asking for three days might be reasonable (--time=3-0)
