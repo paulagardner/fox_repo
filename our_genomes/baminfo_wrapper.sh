edb59f86c82ba77ceec5d9c63d384011138c6322
Paula Gardner <paula.gardner@uea.ac.uk>
2025-03-24 14:11:58 +0000

    adding files from our other data sources- corsac and arctic foxes, as well as dog genome

--- our_genomes/baminfo_wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-##SBATCH --cpus-per-task=1 #should be number of cores you will be using
-##S#BATCH --job-name=myworkflow
-
-
-module load nextflow 
-
-nextflow run bamfile_info.nf -resume -process.echo #\
-    #tee process.txt # tee is not necessary if you submit this as a bash job
