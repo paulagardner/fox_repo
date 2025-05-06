5b5530eebdcd26a6596b3aca51886e51f54d0e0d
Paula Gardner <paula.gardner@uea.ac.uk>
2025-05-06 22:26:51 +0100

    fix eigensoft.sh, add in optional flag to NOT remove outliers and keep the possible other fox species in

--- variant_calling/Eigenstrat/eigensoft.sh
+++ variant_calling/Eigenstrat/eigensoft.sh
@@ -5,11 +5,15 @@ module load gsl #need to load these to get smartpca to work
 module load openblas 
 module load plink/2.00
 
+
+########## I should filetr for missingness much earlier- at this step- so it doesn't have to be done later and then samples
+### with too much missing data removed from the Rscript for PCA via Eigensoft results, going backwards ina  confusing way
+
 plink2 --vcf /gpfs/data/bergstrom/paula/fox_repo/variant_calling/vcf_filtering/autosomes/variant_sites/Vvulpes.c2.samplesites01.vcf.gz \
-        --out Vvulpes_plink_autosomal --keep-allele-order --const-fid 0
+        --make-bed --out Vvulpes_plink_autosomal --keep-allele-order --const-fid 0
 
-smartpca -p par.smartpca
+smartpca -p /gpfs/data/bergstrom/paula/fox_repo/variant_calling/Eigenstrat/par.smartpca
 
-/gpfs/data/bergstrom/sw/bin/fix-smartpca-evec-table.sh Vvulpes.pca.eval > Vvulpestidy.evec
+/gpfs/data/bergstrom/sw/bin/fix-smartpca-evec-table.sh Vvulpes.pca.evec > Vvulpestidy.evec
 
 
